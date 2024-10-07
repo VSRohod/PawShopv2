16313a638fb2ed520ea43cc21774f0bc3ed7e870
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-10-07 12:23:37 -0300

    carrinho

--- assets/js/lojinha.js
+++ assets/js/lojinha.js
@@ -1,5 +1,6 @@
 var catalogo = []
 var carrinho = []
+var valorTotal = 0
 var id = 0;
 
 class Produto {
@@ -12,7 +13,10 @@ class Produto {
     }
 }
 
-catalogo.push(new Produto(0,'#','Coleira','lorem',100))
+catalogo.push(new Produto(0,'../images/products/productImage.png','Coleira','A coleira é o acessório essencial para manter o seu companheiro de quatro patas seguro e com estilo. Feita com materiais de alta qualidade, nossa coleira é resistente e confortável para o seu cão. Com diversos designs e cores disponíveis, você pode encontrar a coleira perfeita que combina com a personalidade do seu peludo amigo. Além de proporcionar segurança durante os passeios, a coleira é uma expressão do amor e cuidado que você tem pelo seu cão.',100))
+catalogo.push(new Produto(1,'../images/products/productImage2.png','Comida','lorem',20))
+catalogo.push(new Produto(2,'../images/products/productImage3.png','Disco de brinquedo','lorem',120))
+catalogo.push(new Produto(3,'../images/products/productImage4.png','Casinha','lorem',200))
 
 function mostrarCatalogo() {
     catalogo.forEach(cadaProduto => {
@@ -23,7 +27,7 @@ function mostrarCatalogo() {
                     <h4 class="card-title" style="color: var(--laranja);">${cadaProduto.titulo}</h4>
                     <p class="card-text">${cadaProduto.desc}</p>
                     <h5 class="card-title" style="color: var(--verde);">R$ ${cadaProduto.valor}</h5>
-                        <button data-bs-toggle="offcanvas" data-bs-target="#carrinho" type="button" class="btn btn-primary">
+                        <button onclick="adicionarAoCarrinho('${cadaProduto.titulo}',${cadaProduto.valor})" data-bs-toggle="offcanvas" data-bs-target="#carrinho" type="button" class="btn btn-primary">
                             COMPRAR
                         </button>
                 </div>
@@ -33,3 +37,24 @@ function mostrarCatalogo() {
 }
 
 mostrarCatalogo()
+
+// CARRINHO
+
+function adicionarAoCarrinho(nomeProduto,valorProduto){
+    document.getElementById('carrinhoLista').innerHTML = ``
+    carrinho.push([nomeProduto,valorProduto])
+
+    carrinho.forEach(cadaProduto => {
+        document.getElementById('carrinhoLista').innerHTML += `
+            <div class="container-fluid d-flex justify-content-between">
+                <h6> ${cadaProduto[0]} </h6>
+                <h6> R$ ${cadaProduto[1]} </h6>
+            </div>
+        `
+    });
+
+    valorTotal = valorTotal + valorProduto
+    document.getElementById('valorTotal').innerHTML = `
+        R$ ${valorTotal}
+    `
+}
