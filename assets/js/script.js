9037740b56b338478a35b8f7e135ca1c7435210e
VSRohod <89645335+VSRohod@users.noreply.github.com>
2024-10-08 12:09:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
+
+window.addEventListener('load', () => {
+    var temaEscolhido = localStorage.getItem('tema')
+
+    if(temaEscolhido == null){
+        localStorage.setItem('tema','light')
+    }
+
+    if(localStorage.getItem('tema') == "dark"){
+        document.querySelector("#botaoTema").checked = true
+        document.querySelector("#temaAtual").innerHTML = `Escuro`
+    }else{
+        document.querySelector("#botaoTema").checked = false
+        document.querySelector("#temaAtual").innerHTML = `Claro`
+    }
+
+    document.querySelector("body").setAttribute('data-bs-theme',localStorage.getItem('tema'))
+})
+
 const botao = document.querySelector("#botaoTema")
 botao.addEventListener("click",modo)
 
-
 function modo(){
-    let body = document.querySelector("body")
-    let temaAtual = document.querySelector("#temaAtual")
 
     if(botao.checked){
-        body.setAttribute("data-bs-theme","dark")
-        temaAtual.innerHTML = "Escuro"
+        localStorage.setItem('tema','dark')
+        document.querySelector("#botaoTema").checked = true
+        document.querySelector("#temaAtual").innerHTML = `Escuro`
+        document.querySelector("body").setAttribute('data-bs-theme','dark')
+
     }else{
-        body.setAttribute("data-bs-theme","light")
-        temaAtual.innerHTML = "Claro"
+        localStorage.setItem('tema','light')
+        document.querySelector("#botaoTema").checked = false
+        document.querySelector("#temaAtual").innerHTML = `Claro`
+        document.querySelector("body").setAttribute('data-bs-theme','light')
+
     }
 }
